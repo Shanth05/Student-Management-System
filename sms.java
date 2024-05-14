d34ea8638c477b35785b28b303f09ba5838bdb9c
shanth05 <shanth.pirashanth5@gmail.com>
2024-05-14 09:02:56 +0530

    FEAT: Dashboard

---
+++ sms.java
@@ -0,0 +1,15 @@
+class main{
+
+    public static void dashboard(){
+        System.out.println("\" Welcome to Developer Stack Student Management System \" \n");
+        System.out.println("1 Save new student");
+        System.out.println("2 Edit student");
+        System.out.println("3 view all student");
+        System.out.println("4 view student");
+        System.out.println("5 Remove student");
+        System.out.println("6 Log out \n");
+    }
+    public static void main(String[] args) {
+        dashboard();
+    }    
+}
